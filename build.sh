2de4e009ad668e116e0e5d074f4ed2b82c537fef
co5m0 <marioconsalvo1@gmail.com>
2025-03-16 18:12:01 +0100

    clean up

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc -s ./markdown/$i.md --template ./template.html -o ./html/$i.html; done
+for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc -s ./markdown/$i.md  --template ./template.html --css=style/base.css -o ./html/$i.html; done
