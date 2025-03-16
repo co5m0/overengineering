3db9489bce54f5a7e9f924d595568b4e5655d65d
co5m0 <marioconsalvo1@gmail.com>
2025-03-17 00:26:06 +0100

    feat: fix seo

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc -s ./markdown/$i.md  --template ./template.html --css=style/base.css -o ./html/$i.html; done
+for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc --from gfm+smart+yaml_metadata_block -s ./markdown/$i.md  --template ./template.html --css=assets/base.css -o ./html/$i.html; done
