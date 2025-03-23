b166984e276f784f3c5d5c0ddf26fe3357196e1d
co5m0 <marioconsalvo1@gmail.com>
2025-03-24 00:17:47 +0100

    feat: week-2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc --from gfm+smart+yaml_metadata_block -s ./markdown/$i.md  --template ./template.html --css=assets/base.css -o ./html/$i.html; done
+for i in $(ls ./markdown | awk -F. '{print $1}'); do echo "build $i"; pandoc --from markdown+smart+yaml_metadata_block -s ./markdown/$i.md  --template ./template.html --css=assets/base.css -o ./html/$i.html; done
--- markdown/index.md
+++ markdown/index.md
@@ -7,6 +7,5 @@ description: co5mo's blog
 - [how to build a blog](./how-to-build-a-blog.html)
 
 ## Weekly
-- [week 1](./week-1.html)
-- [week 2](./week-2.html)
-
+- [week 1 - Starting my personal blog in 2025](./week-1.html)
+- [week 2 - Not a vibe (over)engineer yet](./week-2.html)
